d847e906e002876ef7c80bf17e588d5bfed80d6c
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2024-11-15 14:49:54 +0900

    feat: delete unused file

--- src/main/kotlin/dev/yeseong0412/authtemplate/domain/user/presentation/dto/request/FriendRequest.kt
+++
@@ -1,7 +0,0 @@
-package dev.yeseong0412.authtemplate.domain.user.presentation.dto.request
-
-import com.fasterxml.jackson.annotation.JsonCreator
-
-data class FriendRequest @JsonCreator constructor(
-    val email: String
-)
