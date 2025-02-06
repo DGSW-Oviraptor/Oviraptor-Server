ca8210a01e44083c7ba91d2df644b2d1030956de
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-02-06 16:46:55 +0900

    mail 보낼 때 dto 사용해서 email 주소 받기

--- src/main/kotlin/dev/yeseong0412/authtemplate/domain/mail/presentation/MailController.kt
+++ src/main/kotlin/dev/yeseong0412/authtemplate/domain/mail/presentation/MailController.kt
@@ -1,11 +1,12 @@
 package dev.yeseong0412.authtemplate.domain.mail.presentation
 
+import dev.yeseong0412.authtemplate.domain.mail.presentation.dto.request.SendMailRequest
 import dev.yeseong0412.authtemplate.domain.mail.service.MailService
 import dev.yeseong0412.authtemplate.global.common.BaseResponse
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,8 +18,8 @@ class MailController(
     @Operation(summary = "인증코드 받기")
     @PostMapping
     fun sendMail(
-        @RequestParam email: String
+        @RequestBody request: SendMailRequest
     ): BaseResponse<Unit> {
-        return mailService.sendMail(email)
+        return mailService.sendMail(request.email)
     }
 }
---
+++ src/main/kotlin/dev/yeseong0412/authtemplate/domain/mail/presentation/dto/request/SendMailRequest.kt
@@ -0,0 +1,5 @@
+package dev.yeseong0412.authtemplate.domain.mail.presentation.dto.request
+
+data class SendMailRequest(
+    val email: String = ""
+)
